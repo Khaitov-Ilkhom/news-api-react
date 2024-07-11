3384ca51d21268b4a00791d7f23c812a3b13b35a
Ilhom <ilhom@example.com>
2024-07-11 15:43:58 +0500

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import './App.css'
-import Index from "./routes/index.jsx";
+import { Link, Navigate, Route, Routes } from 'react-router-dom'
+import SingleProductPage from './routes/single-page/SinglePage.jsx'
+import Home from './routes/home/Home'
+import NotFound from "./routes/not-found/NotFound"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-function App() {
-
-
-
+const App = () => {
+  AOS.init();
   return (
-    <div className="app">
-        <Index/>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/single-product/:productId" element={<SingleProductPage />}/>
+        <Route path='not-found' element={<NotFound/>} />
+        <Route path='*' element={<Navigate to='/not-found'/>} />
+      </Routes>
+      <ToastContainer/>
+    </>
   )
 }
 
--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,23 +1,19 @@
-import "./Home.css"
-import {useFetch} from "../../hooks/UseFetch.jsx";
-import Container from "../../components/container/Container.jsx";
-import Cards from "../../components/cards/Cards.jsx";
-import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react'
+import Nav from '../../components/navbar/Nav'
+import AllProduct from '../../components/all-products/AllProduct.jsx'
 
 const Home = () => {
 
-    const [data, loading] = useFetch("top-headlines?sources=techcrunch&apiKey=92fe0548e176495c8e447ef03bb2edd1")
-    return (
-        <Container>
-            <h1 className="bg-amber-400">Home</h1>
-            <div className="cards">
-                {
-                    data?.articles?.map(item =>
-                        <Cards item={item} key={uuidv4()}/>
-                    )
-                }
-            </div>
-        </Container>
-    )
+  const [search, setSearch] = useState("")
+
+  
+ 
+  return (
+    <div>
+        <Nav search={search} setSearch={setSearch}/>
+        <AllProduct/>
+    </div>
+  )
 }
+
 export default Home
